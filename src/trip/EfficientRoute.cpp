c83c7217e22f434b9ff420c0daf5182625efa46c
Chloe Larsen <chloelarsen72@gmail.com>
2026-08-16 14:08:30 +0200

    implementing effcient

--- src/trip/EfficientRoute.cpp
+++ src/trip/EfficientRoute.cpp
@@ -2,4 +2,43 @@
 
 std::vector<Place *> EfficientRoute::pickPlaces(Location *current, Location *destination)
 {
+    // get currents places to root
+    Place *currentParent = current->getParentNode();
+    std::vector<Place *> finalRoute;
+    finalRoute.push_back(current);
+    while (currentParent != nullptr)
+    {
+        finalRoute.push_back(currentParent);
+        currentParent = currentParent->getParentNode();
+    }
+
+    // get des places to root
+    Place *destinationParent = destination->getParentNode();
+    std::vector<Place *> destinationRoute;
+    destinationRoute.push_back(destination);
+    while (destinationParent != nullptr)
+    {
+        destinationRoute.push_back(destinationParent);
+        destinationParent = destinationParent->getParentNode();
+    }
+
+    Place *lastMatch;
+    // Go till top of the two stacks are not the same
+    while (finalRoute.back() == destinationRoute.back())
+    {
+        lastMatch = finalRoute.back();
+        finalRoute.pop_back();
+        destinationRoute.pop_back();
+    }
+    // add last match to the top of the final vector
+    finalRoute.push_back(lastMatch);
+
+    // put destination onto final
+    while (destinationRoute.size() > 0)
+    {
+        finalRoute.push_back(destinationRoute.back());
+        destinationRoute.pop_back();
+    }
+
+    return finalRoute;
 }
